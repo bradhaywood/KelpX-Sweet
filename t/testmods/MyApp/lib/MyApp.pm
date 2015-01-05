e22a8a9b4e7276611665dcf0158be088af753ced
Brad Haywood <brad.haywood@opusvl.com>
2015-01-05 11:44:10 +0000

    Added around test

--- t/testmods/MyApp/lib/MyApp.pm
+++ t/testmods/MyApp/lib/MyApp.pm
@@ -2,3 +2,11 @@ package MyApp;
 
 use KelpX::Sweet;
 maps ['Main'];
+
+around 'build' => sub {
+    my $method = shift;
+    my $self   = shift;
+    $self->routes->add('/aroundtest' => sub { 'Works' });
+    
+    $self->$method(@_);
+};
